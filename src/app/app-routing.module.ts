9346416d0144ebeba0713cab60500ded9f51405f
Mayarabnc <mayarabncabral@gmail.com>
2022-07-08 15:09:03 -0300

    Adicionando rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CardapioComponent } from './cardapio/cardapio.component';
+import { CarrosselComponent } from './carrossel/carrossel.component';
+import { FacaSeuPedidoComponent } from './faca-seu-pedido/faca-seu-pedido.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SobreNosComponent } from './sobre-nos/sobre-nos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"cardapio", component:CardapioComponent},
+  {path:"carrossel", component:CarrosselComponent},
+  {path:"pedido", component:FacaSeuPedidoComponent},
+  {path:"navbar", component:NavbarComponent},
+  {path:"sobre", component:SobreNosComponent} 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
